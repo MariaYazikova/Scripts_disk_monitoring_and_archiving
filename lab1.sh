295b173bd5b7e9b286323bef7503917771a6af7a
vikusido <145844166+vikusido@users.noreply.github.com>
2024-09-30 22:07:06 +0300

    created script

---
+++ lab1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <directory_path>"
+	exit 1
+fi
+
+DIR="$1"
+
+if [ ! -d "$DIR" ]; then
+	echo "Error: this directory path is not a folder or does not exist"
+	exit 1
+fi
+
+USAGE=$(df "$DIR" | awk 'NR==2 {print $5}' | sed 's/%//')
+echo "Usage of '$DIR': $USAGE%"
